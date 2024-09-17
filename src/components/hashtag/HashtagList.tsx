4748044452d996e0192c54632e21cf170baa98de
Andrii <andrii@Andriis-iMac.local>
2024-09-17 15:14:49 +0300

    first commit

---
+++ src/components/hashtag/HashtagList.tsx
@@ -0,0 +1,20 @@
+import { useFeedbackItemsStore } from "../stores/feedbackItemsStore";
+import HashtagItem from "./HashtagItem";
+
+export default function HashtagList() {
+  const companyList = useFeedbackItemsStore((state) => state.getCompanyList());
+  const selectCompany = useFeedbackItemsStore((state) => state.selectCompany);
+  return (
+    <ul className="hashtags">
+      {companyList.map((company) => {
+        return (
+          <HashtagItem
+            key={company}
+            onSelectCompany={selectCompany}
+            company={company}
+          />
+        );
+      })}
+    </ul>
+  );
+}
